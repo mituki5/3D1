c83021fbb28e94771ca34dfd6b50ab3890248d3f
mituki <SDT2395GP0021@edu.sca.ac.jp>
2023-05-20 16:31:57 +0900

    Unity レッスン3.2まで作った

---
+++ Assets/scripts/RepetBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepetBackground : MonoBehaviour
+{
+    Vector3 startPos;//リピートの開始位置
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;//ゲーム開始時の場所を記憶
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //何かの条件が満たされたら…
+        if (startPos.x - transform.position.x > 50.0f)//現在のX座標‐最初のX座標　＞　規定値
+        {
+            transform.position = startPos;//場所をリセット
+        }
+    }
+}
---
+++ Assets/scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //面倒なのでrivate省略してます
+    [SerializeField]GameObject obstaclePrefab;//障害物プレハブ
+    Vector3 spawnPos = new Vector3(25,0,0);//スポーンする場所
+    float elapsedTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;//毎Fの時間を足していく
+        if (elapsedTime > 2.0f)
+        {
+            Instantiate(obstaclePrefab,spawnPos,obstaclePrefab.transform.rotation);
+            elapsedTime = 0.0f;
+        }
+        //Debug.Log(elapsedTime);//デバック
+
+    }
+}
