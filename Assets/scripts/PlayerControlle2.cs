b1e3c9ff3aa2b578c598f0cb750e1af737e32cd2
mituki <SDT2395GP0021@edu.sca.ac.jp>
2023-06-03 13:59:06 +0900

    3.4やった

--- Assets/scripts/PlayerControlle2.cs
+++ Assets/scripts/PlayerControlle2.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerControlle2 : MonoBehaviour
 {
+    [SerializeField] ParticleSystem explosionParticle;
+    [SerializeField] ParticleSystem dirtParticle;
     Rigidbody rb;
     [SerializeField]float gravityModifier;//privateは省略
     [SerializeField]float jumpForce;//ジャンプ力
@@ -17,6 +19,7 @@ public class PlayerControlle2 : MonoBehaviour
         //rb.AddForce(Vector3.up * 1000);//上え飛ばす
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
+        //dirtParticle.Play();//最初から再生
     }
 
     // Update is called once per frame
@@ -28,6 +31,7 @@ public class PlayerControlle2 : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce ,ForceMode.Impulse);//上へ力を加える
             isOnGround = false;//ジャンプした＝地面にいない
             playerAnim.SetTrigger("Jump_trig");//ジャンプアニメ発動準備
+            dirtParticle.Stop();
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -35,13 +39,16 @@ public class PlayerControlle2 : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;//地面についている状態に変更
+            dirtParticle.Play();
         }
-        else if (collision.gameObject.CompareTag("Obstacle"))
+        else if (collision.gameObject.CompareTag("Obstacle"))//障害物に衝突
         {
             gameOver = true;//ゲームオーバーにしてみる
+            explosionParticle.Play();//再生
             //Debug.Log("Game Over!");//プレイ中にConsoleに表示される
             playerAnim.SetBool("Death_b", true);//ここで死亡状態ｂにする。（Death_b とかいうのは本来自分で定義できる）
             playerAnim.SetInteger("DeathType_int", 1);//integerは整数の意味。死亡のタイプ？を一番目のやつにする的な。
+            dirtParticle.Stop();//砂のアニメは消す
         }    
     }
 }
